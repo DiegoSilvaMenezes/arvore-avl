7bb7ee1abe3f82365913b1b7e602f3ce243f3de5
DiegoSilvaMenezes <112735462+DiegoSilvaMenezes@users.noreply.github.com>
2025-04-23 19:44:56 -0300

    implementar classe No

---
+++ ArvoreAVL.java
@@ -0,0 +1,12 @@
+public class No {
+    int valor;
+    int altura;
+    No esquerdo;
+    No direito;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.altura = 1;
+    }
+}
+
